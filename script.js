49a0a39d47bf369bc94af6a6aebe5ee27bad94a0
Hozifa503 <prog.hozifa@gmail.com>
2026-08-28 20:10:14 +0300

    add dataURLtoBlob helper function for base64 conversion

--- script.js
+++ script.js
@@ -148,3 +148,15 @@ async function initPlayer() {
     audio.volume = 0.7;
     updateVolumeUI();
 }
+
+function dataURLtoBlob(dataURL) {
+    const parts = dataURL.split(',');
+    const mime = parts[0].match(/:(.*?);/)[1];
+    const bstr = atob(parts[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+}
